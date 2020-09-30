290cad2d98d049a37b4ec9a70e5e2354eed3e8ec
jiteshchawla1 <jiteshchawla1511@gmail.com>
2020-09-30 20:30:20 +0530

    Add files via upload

---
+++ matplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+x1=[1,2,3]
+y1=[4,5,6]
+plt.plot(x1,y1,label="first line")
+x2=[5,8,9]
+y2=[2,3,1]
+pltplot(x2,y2,label="second line")
+plt.xlabel("plot number ")
+plt.ylabel("new variables")
+pltlegend()
+plt.show()
